014d80df74264ddb231a8997fbfd928dd7a9b1bf
Benjamin Morrow <morrowbenjamin42@gmail.com>
2022-10-12 16:58:09 -0400

    fixed some weird unintended behavior

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Line } from "rc-progress";
 import {
   IconCheck,
   IconDownload,
@@ -7,41 +6,41 @@ import {
   IconBrandGithub,
   IconBrandTwitter,
 } from "@tabler/icons";
-import { useEffect, useState } from "react";
 
 export default function Sidebar({ status }: { status: boolean }) {
   return (
-      <aside
-        className={`ease-in-out duration-500s w-0 lg:w-72 ${
-          status ? "showBar navbarActive" : ""
-        }`}
-      >
-        {/* Header */}
-        <div className="h-60 p-8 header">
-          <div className="h-24 mb-4 flex justify-center">
-            <a href="https://www.linkedin.com/in/morrowbenjamin/">
-              <Image
-                src="/face.png"
-                alt="Picture of my face"
-                className="rounded-full"
-                width={96}
-                height={96}
-              />
-            </a>
-          </div>
-          <h5 className="mb-2.5 text-center text-lg name">
-            <a>Benjamin Morrow</a>
-          </h5>
-          <div className="small-text text-center text-sm">
-            Front-end Developer
-            <br />
-            UI/UX Designer
-          </div>
+    <aside
+      className={`ease-in-out duration-500s w-0 lg:w-96 ${
+        status ? "showBar navbarActive" : ""
+      }`}
+    >
+      {/* Header */}
+      <div className="h-60 p-8 header">
+        <div className="h-24 mb-4 flex justify-center">
+          <a href="https://www.linkedin.com/in/morrowbenjamin/">
+            <Image
+              src="/face.png"
+              alt="Picture of my face"
+              className="rounded-full"
+              width={96}
+              height={96}
+            />
+          </a>
         </div>
-        <div className="sidebarFrame">
-          {/* Scroll Area */}
-          <div className="scrollContent overflow-y-scroll">
-            <div className="divide-solid divide-y w-max">
+        <h5 className="mb-2.5 text-center text-lg name">
+          <a>Benjamin Morrow</a>
+        </h5>
+        <div className="small-text text-center text-sm">
+          Front-end Developer
+          <br />
+          UI/UX Designer
+        </div>
+      </div>
+      <div className="sidebarFrame justify-center">
+        {/* Scroll Area */}
+        <div className="scrollContent">
+          <div className="divide-solid divide-y">
+            <div>
               <div className="infoTable py-4">
                 <ul>
                   <li>
@@ -58,62 +57,73 @@ export default function Sidebar({ status }: { status: boolean }) {
                   </li>
                 </ul>
               </div>
-              <div className="languageSkills pt-2">
-                  <div className="pb-1 languageHeading">
-                    <span className="text-lg">Typescript</span>
-                    <span className="text-lg">Javascript</span>
-                  </div>
-                  <div className="pb-1">
-                    <h6 className="text-lg">Javascript</h6>
-                  </div>
-                  <div className="pb-1">
-                    <h6 className="text-lg">React</h6>
-                  </div>
-                  <div className="pb-1">
-                    <h6 className="text-lg">MySQL</h6>
-                  </div>
-                  <div className="languageBar"></div>
-                <div className="librariesList pt-4 mb-4">
-                  <ul>
-                    <li>
-                      <IconCheck color="#ffc108" size={24} />
-                      <span>Bootstrap, Materialize, Tailwind</span>
-                    </li>
-                    <li>
-                      <IconCheck color="#ffc108" size={24} />
-                      <span>SQL and NoSQL DB Knowledge</span>
-                    </li>
-                    <li>
-                      <IconCheck color="#ffc108" size={24} />
-                      <span>Responsive Design Paradigms</span>
-                    </li>
-                    <li>
-                      <IconCheck color="#ffc108" size={24} />
-                      <span>Git Knowledge</span>
-                    </li>
-                  </ul>
+            </div>
+            <div>
+              <div className="languageSkills pt-2 ">
+                <div className="pb-1 text-center">
+                  <span className="text-lg text-center">Languages</span>
                 </div>
-                <div className="cvDownload py-4 ">
-                  <a href="/cv.pdf" download className="flex">
-                    <span className="pr-2 cvLink">Download CV</span>
-                    <IconDownload color="#ffc108" size={20} />
-                  </a>
+                <div className="pb-1 languageSplit">
+                  <span className="text-lg">Typescript</span>
+                  <span className="text-lg">Javascript</span>
                 </div>
+                <div className="pb-1 languageSplit">
+                  <span className="text-lg">Python</span>
+                  <span className="text-lg">React</span>
+                </div>
+                <div className="pb-1"></div>
+              </div>
+            </div>
+            <div>
+              <div className="librariesList pt-4 mb-4">
+                <ul>
+                  <li>
+                    <IconCheck color="#ffc108" size={24} />
+                    <span>Bootstrap, Materialize, Tailwind CSS</span>
+                  </li>
+                  <li>
+                    <IconCheck color="#ffc108" size={24} />
+                    <span>SQL and NoSQL DB Knowledge</span>
+                  </li>
+                  <li>
+                    <IconCheck color="#ffc108" size={24} />
+                    <span>Responsive Design Paradigms</span>
+                  </li>
+                  <li>
+                    <IconCheck color="#ffc108" size={24} />
+                    <span>Git Knowledge</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="cvDownload py-4 ">
+                <a href="/cv.pdf" download className="flex">
+                  <span className="pr-2 cvLink">Download CV</span>
+                  <IconDownload color="#ffc108" size={20} />
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="socials px-9 py-auto h-14 flex justify-between items-center">
+      </div>
+      <div className="socials px-9 py-auto h-14 flex justify-between items-center">
+        <div>
           <a href="https://www.linkedin.com/in/morrowbenjamin/">
             <IconBrandLinkedin size={30} />
           </a>
+        </div>
+        <div>
           <a href="https://github.com/TheResinger">
             <IconBrandGithub size={30} />
           </a>
+        </div>
+        <div>
           <a href="https://twitter.com/TheRezinger">
             <IconBrandTwitter size={30} />
           </a>
         </div>
-      </aside>
+      </div>
+    </aside>
   );
 }
