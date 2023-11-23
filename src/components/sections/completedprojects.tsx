e3fbce97c285148b1a46a6dd45348415c1e362f4
TheResinger <mororwbenjamin42@gmail.com>
2023-11-23 03:49:45 -0500

    moved the top section to its own file

--- src/components/sections/completedprojects.tsx
+++ src/components/sections/completedprojects.tsx
@@ -1,15 +1,17 @@
 import Carousel from '../subcomponents/carousel'
 import { forwardRef } from 'react'
 
-const CompletedProjects = forwardRef<HTMLElement>(({}, ref) => {
+const CompletedProjects = forwardRef<HTMLDivElement>((props, ref): JSX.Element => {
   return (
-    <section id="completedProjects" className="pb-4 pt-2" ref={ref}>
-      <div className="grid grid-cols-3 md:grid-cols-4 px-3 pb-4">
-        <div className="col-start-1 col-end-5">
-          <h5 className="text-2xl text-center">Completed Projects</h5>
+    <section id="completedProjects" >
+      <div className="pb-4 pt-2" ref={ref} id="completedProjects">
+        <div className="grid grid-cols-3 md:grid-cols-4 px-3 pb-4">
+          <div className="col-start-1 col-end-5">
+            <h5 className="text-2xl text-center">Completed Projects</h5>
+          </div>
         </div>
+        <Carousel />
       </div>
-      <Carousel />
     </section>
   )
 })
